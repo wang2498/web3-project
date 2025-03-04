40d95319dfeb033884e961dfb8692b222be25627
wang <wang2498163@163.com>
2025-03-04 15:50:38 +0800

    feat: dev

--- chef-stake/contract/hardhat.config.ts
+++ chef-stake/contract/hardhat.config.ts
@@ -2,8 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const INFURA_API_KEY = "3630a2e3f2a84bf997ab2d931011233b";
-const SEPOLIA_PRIVATE_KEY =
-  "9e8dd536c43390d86994d0c13eb1bcff9a507bdd0b2e3586aa6894b03875ded1";
+const SEPOLIA_PRIVATE_KEY = "";
 const ETHERSCAN_API_KEY = "mAmWJvdlz8GbFEKlrBzkoP+SV6yEVYmc5DmkKJVBXOc2WJ7mG0SCWQ";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
